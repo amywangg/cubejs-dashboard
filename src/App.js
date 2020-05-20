6678a51574866ac5fe4bbf7a7ca0ccd04b9a6986
Amy <amy_wangg@hotmail.com>
2020-05-20 01:15:14 -0400

    Playing around with CubeJS react dashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./body.css";
+import "antd/dist/antd.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { Layout } from "antd";
+import cubejs from "@cubejs-client/core";
+import { CubeProvider } from "@cubejs-client/react";
+import client from "./graphql/client";
+import Header from "./components/Header";
+const API_URL = "http://localhost:4000";
+const CUBEJS_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODk5MDkwNjUsImV4cCI6MTU4OTk5NTQ2NX0.ak-CkIKzvL52xnXfKqHuQ_eok0ejo7H0eRnQmJNbgQ4";
+const cubejsApi = cubejs(CUBEJS_TOKEN, {
+  apiUrl: `${API_URL}/cubejs-api/v1`,
+});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppLayout = ({ children }) => (
+  <Layout
+    style={{
+      height: "100%",
+    }}
+  >
+    <Header />
+    <Layout.Content>{children}</Layout.Content>
+  </Layout>
+);
+
+const App = ({ children }) => (
+  <CubeProvider cubejsApi={cubejsApi}>
+    <ApolloProvider client={client}>
+      <AppLayout>{children}</AppLayout>
+    </ApolloProvider>
+  </CubeProvider>
+);
 
 export default App;
