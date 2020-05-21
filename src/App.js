98f2d9c4e36ed469143aef9df219f85924f1e55d
Amy <amy_wangg@hotmail.com>
2020-05-21 12:35:22 -0400

    working security check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "./body.css";
 import "antd/dist/antd.css";
@@ -10,11 +9,25 @@ import { CubeProvider } from "@cubejs-client/react";
 import client from "./graphql/client";
 import Header from "./components/Header";
 const API_URL = "http://localhost:4000";
-const CUBEJS_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODk5MDkwNjUsImV4cCI6MTU4OTk5NTQ2NX0.ak-CkIKzvL52xnXfKqHuQ_eok0ejo7H0eRnQmJNbgQ4";
-const cubejsApi = cubejs(CUBEJS_TOKEN, {
-  apiUrl: `${API_URL}/cubejs-api/v1`,
-});
+// const CUBEJS_TOKEN =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODk5MDkwNjUsImV4cCI6MTU4OTk5NTQ2NX0.ak-CkIKzvL52xnXfKqHuQ_eok0ejo7H0eRnQmJNbgQ4";
+
+let apiTokenPromise;
+
+const cubejsApi = cubejs(
+  () => {
+    if (!apiTokenPromise) {
+      // pass in query parameters for filtering
+      apiTokenPromise = fetch(`${API_URL}/auth/cubejs-token?user=1`)
+        .then((res) => res.json())
+        .then((r) => r.token);
+    }
+    return apiTokenPromise;
+  },
+  {
+    apiUrl: `${API_URL}/cubejs-api/v1`,
+  }
+);
 
 const AppLayout = ({ children }) => (
   <Layout
